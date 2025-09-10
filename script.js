fd8035e284aa34c6aa4b4cda448d7e4c4d0cef0f
Murilo <renatomumu14@gmail.com>
2025-09-10 16:53:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function cadastro() {
+    alert("Cadastro feito com sucesso")
+}
+function login() {
+    var usuario = document.getElementById('user').value;
+    var senha = document.getElementById('senha').value;
+    if(usuario == "admin" && senha == "1234"){
+        window.location = "./inicial.html"
+    }else {
+        alert("Usuário/Senha incorreta! Tente novamente!")
+    }
+        
+}
